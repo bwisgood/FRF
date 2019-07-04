1397e15b4f039e83f6790f5982d169a5b14e4693
bwisgood <bwisgood@163.com>
2019-07-04 14:05:03 +0800

    模板

---
+++ v2/serializer.py
@@ -0,0 +1,212 @@
+from inspect import ismethod
+from flask import current_app
+from datetime import datetime
+from .exceptions import *
+from sqlalchemy.exc import SQLAlchemyError
+
+
+class Serializer(object):
+    model_class = None
+    fields = "__all__"
+    extend_fields = None
+    logical_delete = {"is_delete": 0}
+    data_time_format = "%Y/%m/%d %H:%M:%S"
+    _map = {
+        "integer": int,
+        "small_integer": int,
+        "boolean": bool,
+        "string": str,
+        # "datetime": datetime,
+        # "date": date,
+        "float": float
+        # "time"
+    }
+
+    def __init__(self):
+        """
+        self.relations： 所有relations
+        self.model_class： 序列化的类对象
+        self.table: sqlalchemy的table对象
+        self.foreign_keys： 所有外键
+        self.extra_func：额外的方法
+        self.all_fields：所有字段
+        """
+        assert self.model_class is not None, "serializer_obj can not be None"
+        self.relations = []
+        self.table = self.model_class.__dict__.get('__table__')
+        self.foreign_keys = self.get_all_foreign_keys()
+        self.extra_func = [ismethod(getattr(self.model_class, attr[0], None)) for attr in
+                           self.model_class.__dict__.items()]
+        self.all_fields = self.get_all_fields()
+
+        self.db = current_app.db
+        if not self.db:
+            raise ImportError("未找到sqlalchemy,请确认是否已安装sqlalchemy"
+                              "并将本extension的初始化置于sqlalchemy初始化之后")
+
+    @classmethod
+    def get_all_fields(cls):
+        """
+        获取所有字段
+        :return:
+        """
+        all_attr_str_list = list(cls.model_class.__dict__.get("__table__").c)
+        all_fields = [str(c).split(".")[1] for c in all_attr_str_list]
+        return all_fields
+
+    def get_all_foreign_keys(self):
+        """
+        获取所有字段
+        :return:
+        """
+        tmp = []
+        fks = self.table.foreign_keys
+        for fk in fks:
+            tmp.append(fk.parent.name)
+        return tmp
+
+    def add_extend_fields(self, return_data):
+        if self.extend_fields is None:
+            return return_data
+        for item in self.extend_fields.items():
+            # 查询值
+            data = return_data.get(item[0])
+            if data is not None:
+                return_data.pop(item[0])
+            else:
+                continue
+            extra_data = item[1].query.filter_by(**{"id": data}).first()
+
+            extend_fields_in = getattr(self, "extend_fields_" + item[0], None)
+            if extend_fields_in:
+
+                return_data1 = dict(
+                    map(self.mapping_func,
+                        dict(filter(lambda x: x[0] in extend_fields_in, extra_data.__dict__.items())).items()))
+            else:
+                return_data1 = dict(
+                    map(self.mapping_func,
+                        dict(filter(lambda x: not x[0].startswith("_"), extra_data.__dict__.items())).items()))
+            return_data.update(**{item[0]: return_data1})
+        return return_data
+
+    @staticmethod
+    def mapping_func(y):
+        if isinstance(y[1], datetime):
+            temp = y[1].strftime("%Y/%m/%d %H:%M:%S")
+            return y[0], temp
+        else:
+            return y
+
+    def handle_return_data(self, data):
+        return data
+
+    def serialize(self, instance):
+        """
+        序列化返回值
+        :param data: 参数
+        :return: 参数data
+        """
+
+        data = self.to_serializer_able(instance)
+
+        if self.fields == '__all__':
+
+            return_data = dict(map(self.mapping_func, data.items()))
+        else:
+            assert isinstance(self.fields, list)
+            return_data = dict(
+                map(self.mapping_func, dict(filter(lambda x: x[0] in self.fields, data.items())).items()))
+        return_data = self.add_extend_fields(return_data)
+        return_data = self.handle_return_data(return_data)
+        return return_data
+
+    def to_serializer_able(self, instance):
+        data_ = {}
+        for field in self.all_fields:
+            data_[field] = getattr(instance, field, None)
+        return data_
+
+    def create(self, data):
+        # 先做排除无用数据
+        data = self.filter_table_data(data)
+        # 再拿去做校验和类型转换
+        data = self.validate(data)
+
+        # 创建实例
+        instance = self.model_class()
+        # 将data的值传递给实例赋值
+        # print(data)
+        for k, v in data.items():
+            setattr(instance, k, v)
+        # 保存instance
+        serialized_data = self.save(instance)
+        self.commit()
+
+        # 返回序列化之后的数据
+        return serialized_data
+
+    def validate(self, data):
+        # 校验参数
+        for field in self.all_fields:
+            # 先非空校验
+            _column = getattr(self.model_class, field, None)
+            match_data = data.get(_column)
+            self.check_field_not_none(_column, match_data)
+            # 再做类型校验
+            self.check_field_type(_column, match_data, data)
+        return data
+
+    def check_field_not_none(self, _column, match_data):
+        if _column.nullable is False and match_data is None and not _column.primary_key:
+            raise ArgumentException
+
+    def check_field_type(self, _column, match_data, data):
+        if match_data is not None:
+            column_type = _column.type.__visit_name__
+            if column_type in self._map:
+                incident_type = self._map.get(column_type)
+                try:
+                    # 用转换过的类型做一次匹配
+                    data[_column] = incident_type(match_data)
+                except Exception:
+                    raise ArgumentException
+
+    def filter_table_data(self, data):
+        # 过滤一遍data，将不属于table的属性排除
+        assert isinstance(data, dict)
+        data = dict(filter(lambda x: x[0] in self.all_fields, data.items()))
+        return data
+
+    def modify(self, instance, data):
+        # 先过滤一遍data，将不属于table的属性排除
+        data = self.filter_table_data(data)
+        # 在做一次参数校验
+        data = self.validate(data)
+        for k, v in data:
+            setattr(instance, k, v)
+        self.commit()
+        return self.serialize(instance)
+
+    def commit(self):
+        try:
+            self.db.session.commit()
+        except Exception:
+            self.db.session.rollback()
+            raise SQLAlchemyError
+
+    def save(self, instance):
+        self.db.session.add(instance)
+
+        return self.serialize(instance)
+
+    def delete(self, instance):
+        # 判断是否需要逻辑删除
+        data = self.serialize(instance)
+        if not self.logical_delete:
+            self.db.session.delete(instance)
+        else:
+            for k, v in self.logical_delete:
+                setattr(instance, k, v)
+        self.commit()
+        return data
--- v2/wrapper.py
+++ v2/wrapper.py
@@ -1,12 +1,11 @@
 from functools import wraps
 from flask import request, jsonify, current_app
 from flask.views import MethodView
-from inspect import ismethod
 from v2.response_code import RET
-from datetime import datetime
 from v2.exceptions import *
 from sqlalchemy.exc import SQLAlchemyError
 from inspect import isfunction
+from v2.docs import DocDataCollector
 
 
 def request_wrapper(func):
@@ -25,6 +24,8 @@ class FlaskRestFramework(object):
     初始化程序时使用的类，用来写内部需要的一些app的runtime variable
     """
 
+    used_cls = []
+
     def __init__(self, app=None, **kwargs):
         self.app = app
         self.db = None
@@ -32,6 +33,9 @@ class FlaskRestFramework(object):
 
         if app is not None:
             self.init_app(app, **kwargs)
+
+        self.doc_data_collector = DocDataCollector()
+        self.generate_html_interface_docs()
 
     def init_app(self, app, **kwargs):
         self.app = app
@@ -45,6 +49,68 @@ class FlaskRestFramework(object):
                               "并将本extension的初始化置于sqlalchemy初始化之后")
 
     # todo usual logger
+    # html api
+    def generate_html_interface_docs(self):
+        from flask import render_template_string
+        if self.app.config["DEBUG"] is True:
+            @self.app.route("/docs")
+            def generate_docs():
+                with open("show_api.html") as f:
+                    s = f.read()
+                return render_template_string(s, all_data=self.doc_data_collector.data)
+
+    def collect_sub_class_use_frf(self):
+        """收集所有使用frf的View"""
+        """
+        需要的东西：
+            1.接口的路由
+            2.接口支持的method
+            3.每种方法的传入参数
+            4.每种方法的返回参数
+            
+        
+        1.收集所有使用frf的view
+        2.去urlmap中寻找对应的view 如果没找到则排除掉
+        
+        """
+        """
+        方案1：
+            使用一个装饰器来捕获所有用到的view ✔️
+        方案2：
+            去app.view_function中遍历出来所有继承ApiView的function
+        """
+        base = {}
+        from inspect import isclass
+        for endpoint, func in self.app.view_functions.items():
+            if not isclass(func):
+                continue
+            if issubclass(func, ApiView):
+                base[endpoint] = func
+
+    def get_app_url_map(self):
+        rl = []
+        sl = []
+        from v2.docs import BaseDocData
+        for i in list(self.app.url_map.iter_rules()):
+            bs = {}
+            bs["endpoint"] = i.endpoint
+            view_func = self.app.view_functions.get(i.endpoint)
+            bs["view_func"] = view_func
+            bs["rule"] = i.rule
+            bs["methods"] = i.methods
+            try:
+                if issubclass(view_func, ApiView):
+                    base_data = BaseDocData(**bs)
+                    self.doc_data_collector.add(base_data)
+                    rl.append(bs)
+                else:
+                    sl.append(bs)
+            except TypeError as e:
+                print(e)
+                sl.append(bs)
+
+        print(rl)
+        print(sl)
 
 
 class BaseView(type):
@@ -333,218 +399,46 @@ class DeleteView(ApiView):
         return jsonify(code=RET.OK, msg='删除成功', data=data)
 
 
-class Serializer(object):
-    model_class = None
-    fields = "__all__"
-    extend_fields = None
-    logical_delete = {"is_delete": 0}
-
-    _map = {
-        "integer": int,
-        "small_integer": int,
-        "boolean": bool,
-        "string": str,
-        # "datetime": datetime,
-        # "date": date,
-        "float": float
-        # "time"
-    }
-
-    def __init__(self):
-        """
-        self.relations： 所有relations
-        self.model_class： 序列化的类对象
-        self.table: sqlalchemy的table对象
-        self.foreign_keys： 所有外键
-        self.extra_func：额外的方法
-        self.all_fields：所有字段
-        """
-        assert self.model_class is not None, "serializer_obj can not be None"
-        self.relations = []
-        self.table = self.model_class.__dict__.get('__table__')
-        self.foreign_keys = self.get_all_foreign_keys()
-        self.extra_func = [ismethod(getattr(self.model_class, attr[0], None)) for attr in
-                           self.model_class.__dict__.items()]
-        self.all_fields = self.get_all_fields()
-
-        self.db = current_app.db
-        if not self.db:
-            raise ImportError("未找到sqlalchemy,请确认是否已安装sqlalchemy"
-                              "并将本extension的初始化置于sqlalchemy初始化之后")
-
-    def get_all_fields(self):
-        """
-        获取所有字段
-        :return:
-        """
-        all_attr_str_list = list(self.table.c)
-        all_fields = [str(c).split(".")[1] for c in all_attr_str_list]
-        return all_fields
-
-    def get_all_foreign_keys(self):
-        """
-        获取所有字段
-        :return:
-        """
-        tmp = []
-        fks = self.table.foreign_keys
-        for fk in fks:
-            tmp.append(fk.parent.name)
-        return tmp
-
-    def add_extend_fields(self, return_data):
-        if self.extend_fields is None:
-            return return_data
-        for item in self.extend_fields.items():
-            # 查询值
-            data = return_data.get(item[0])
-            if data is not None:
-                return_data.pop(item[0])
-            else:
-                continue
-            extra_data = item[1].query.filter_by(**{"id": data}).first()
-
-            extend_fields_in = getattr(self, "extend_fields_" + item[0], None)
-            if extend_fields_in:
-
-                return_data1 = dict(
-                    map(self.mapping_func,
-                        dict(filter(lambda x: x[0] in extend_fields_in, extra_data.__dict__.items())).items()))
-            else:
-                return_data1 = dict(
-                    map(self.mapping_func,
-                        dict(filter(lambda x: not x[0].startswith("_"), extra_data.__dict__.items())).items()))
-            return_data.update(**{item[0]: return_data1})
-        return return_data
-
-    @staticmethod
-    def mapping_func(y):
-        if isinstance(y[1], datetime):
-            temp = y[1].strftime("%Y/%m/%d %H:%M:%S")
-            return y[0], temp
-        else:
-            return y
-
-    def handle_return_data(self, data):
-        return data
-
-    def serialize(self, instance):
-        """
-        序列化返回值
-        :param data: 参数
-        :return: 参数data
-        """
-
-        data = self.to_serializer_able(instance)
-
-        if self.fields == '__all__':
-
-            return_data = dict(map(self.mapping_func, data.items()))
-        else:
-            assert isinstance(self.fields, list)
-            return_data = dict(
-                map(self.mapping_func, dict(filter(lambda x: x[0] in self.fields, data.items())).items()))
-        return_data = self.add_extend_fields(return_data)
-        return_data = self.handle_return_data(return_data)
-        return return_data
-
-    def to_serializer_able(self, instance):
-        data_ = {}
-        for field in self.all_fields:
-            data_[field] = getattr(instance, field, None)
-        return data_
-
-    def create(self, data):
-        # 先做排除无用数据
-        data = self.filter_table_data(data)
-        # 再拿去做校验和类型转换
-        data = self.validate(data)
-
-        # 创建实例
-        instance = self.model_class()
-        # 将data的值传递给实例赋值
-        # print(data)
-        for k, v in data.items():
-            setattr(instance, k, v)
-        # 保存instance
-        serialized_data = self.save(instance)
-        self.commit()
-
-        # 返回序列化之后的数据
-        return serialized_data
-
-    def validate(self, data):
-        # 校验参数
-        for field in self.all_fields:
-            # 先非空校验
-            _column = getattr(self.model_class, field, None)
-            match_data = data.get(_column)
-            self.check_field_not_none(_column, match_data)
-            # 再做类型校验
-            self.check_field_type(_column, match_data, data)
-        return data
-
-    def check_field_not_none(self, _column, match_data):
-        if _column.nullable is False and match_data is None and not _column.primary_key:
-            raise ArgumentException
-
-    def check_field_type(self, _column, match_data, data):
-        if match_data is not None:
-            column_type = _column.type.__visit_name__
-            if column_type in self._map:
-                incident_type = self._map.get(column_type)
-                try:
-                    # 用转换过的类型做一次匹配
-                    data[_column] = incident_type(match_data)
-                except Exception:
-                    raise ArgumentException
-
-    def filter_table_data(self, data):
-        # 过滤一遍data，将不属于table的属性排除
-        assert isinstance(data, dict)
-        data = dict(filter(lambda x: x[0] in self.all_fields, data.items()))
-        return data
-
-    def modify(self, instance, data):
-        # 先过滤一遍data，将不属于table的属性排除
-        data = self.filter_table_data(data)
-        # 在做一次参数校验
-        data = self.validate(data)
-        for k, v in data:
-            setattr(instance, k, v)
-        self.commit()
-        return self.serialize(instance)
-
-    def commit(self):
-        try:
-            self.db.session.commit()
-        except Exception:
-            self.db.session.rollback()
-            raise SQLAlchemyError
-
-    def save(self, instance):
-        self.db.session.add(instance)
-
-        return self.serialize(instance)
-
-    def delete(self, instance):
-        # 判断是否需要逻辑删除
-        data = self.serialize(instance)
-        if not self.logical_delete:
-            self.db.session.delete(instance)
-        else:
-            for k, v in self.logical_delete:
-                setattr(instance, k, v)
-        self.commit()
-        return data
-
-
 class ExceptionHandler(object):
     """处理运行中的异常"""
     pass
 
 
+if __name__ == '__main__':
+    from flask import Flask
+
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = r'mysql+pymysql://root:mysql@127.0.0.1:3306/ai_community_v3'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+    app.config['SQLALCHEMY_ECHO'] = True
+    app.config['DEBUG'] = True
+
+    from flask_sqlalchemy import SQLAlchemy
+
+    db = SQLAlchemy(app)
+    frf = FlaskRestFramework(app)
+
+    from v2.serializer import Serializer
 
 
+    class AModel(db.Model):
+        __tablename__ = "amodel"
+        id = db.Column(db.Integer, primary_key=True)
+        name = db.Column(db.String(30))
+        address = db.Column(db.String(30))
 
 
+    class ASerialzier(Serializer):
+        model_class = AModel
+
+
+    class A(GetView, PostView):
+        serializer = ASerialzier
+        look_up = ("name", "address")
+
+
+    app.add_url_rule("/a", view_func=A)
+
+    frf.get_app_url_map()
+    # frf.collect_sub_class_use_frf()
+    app.run(debug=True)
